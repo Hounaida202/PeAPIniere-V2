b49d72fdf19514ed4461765e816de820523f401e
Hounaida202 <etrichih@gmail.com>
2025-04-17 21:12:56 +0000

    recuperer les plantes selon les categoris

--- mon-app/src/pages/PlantList.jsx
+++ mon-app/src/pages/PlantList.jsx
@@ -6,4 +6,20 @@ const PlantList = ({ categorieId }) => {
   const [plants, setPlants] = useState([]);
   const [error, setError] = useState('');
 
+  useEffect(() => {
+    const fetchPlants = async () => {
+      try {
+        const res = await axios.get(`/getplantes/${categorieId}`);
+        setPlants(res.data.plantes); 
+      } catch (err) {
+        console.error(err);
+        setError("errer");
+      }
+    };
+
+    if (categorieId) {
+      fetchPlants();
+    }
+  }, [categorieId]);
+
   
